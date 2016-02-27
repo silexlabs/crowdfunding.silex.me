69259684848a6dbfe146a17ffdf32fcb9eb72533
singchan <billy321@im-paris.fr>
2016-02-27 21:13:11 +0100

    Create js/script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 
-        /* */
+        $(window).load(function(){
+            $("body").addClass("loaded");
+            return;
+            $("head").append(`
+            <style>
+                @media (min-width: 1000px) {
+                @font-face {
+                  font-family: 'Jura';
+                  font-style: normal;
+                  font-weight: 300;
+                  src: local('Jura Light'), local('Jura-Light'), url('assets/jura.woff2') format('woff2');
+                  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
+                }
+                }
+            </style>
+            `);
+        })
     
